8b2747dad462618d784454b39cf939d042867c0c
aneeshpatne <aneeshpatne@gmail.com>
2025-03-07 13:35:05 +0530

    replace Geist font with Inter in layout.js and update body class

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,16 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
+  variable: "--font-inter",
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "LifeSpan",
   description: "The AI-Powered ",
@@ -19,11 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
